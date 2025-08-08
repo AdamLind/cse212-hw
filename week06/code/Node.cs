a6814bf416ce7b9c703af7baac25db5683894347
Adam Lind <adamsplind@gmail.com>
2025-08-07 20:52:28 -0600

    finish w06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,14 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        // I figured that since we're already making a contains function, 
+        // we can use it to check if the value already exists before
+        // inserting it, to avoid duplicates.
+        if (Contains(value))
+        {
+            // Value already exists in the tree
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +42,25 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            return Left?.Contains(value) ?? false;
+        }
+        else
+        {
+            return Right?.Contains(value) ?? false;
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
